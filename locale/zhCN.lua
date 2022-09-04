5aff2812cbf0a8c4407fa777f62d67c5b646e668
T3402 <39285160+Xiaoxfit@users.noreply.github.com>
2022-09-04 10:54:11 +0800

    Update zhCN.lua

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -1,9 +1,9 @@
 local L = select(2, ...).L('zhCN')
 
 -- Config
--- L['Only show for tracked quests'] = ''
--- L['Only show for quests in current zone'] = ''
--- L['Distance in yards how far away the quest can be'] = ''
--- L['Up'] = ''
--- L['Down'] = ''
--- L['Both'] = ''
+-- L['Only show for tracked quests'] = '仅显示跟踪任务'
+-- L['Only show for quests in current zone'] = '仅显示当前区域中的任务'
+-- L['Distance in yards how far away the quest can be'] = '距离，以码为单位，任务可以有多远'
+-- L['Up'] = '弹起'
+-- L['Down'] = '按下'
+-- L['Both'] = '两者都'
